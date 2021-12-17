429592722321966f912ab535fb1ffcdf9bcdcb42
heo-dae-won <heodaewon92@kakao.com>
2021-12-17 10:56:17 +0900

    first commit

---
+++ src/main/java/com/heo/dae/won/subscribeperminute/SubscribePerMinuteApplication.java
@@ -0,0 +1,13 @@
+package com.heo.dae.won.subscribeperminute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribePerMinuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscribePerMinuteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heo/dae/won/subscribeperminute/SubscribePerMinuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heo.dae.won.subscribeperminute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribePerMinuteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
